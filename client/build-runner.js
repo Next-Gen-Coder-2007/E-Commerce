a9111365f680f91f19c5d14149a82bb90654a0b3
Subash B <subash59245@gmail.com>
2026-09-03 12:03:31 +0530

    chore: streamline 1-click vercel deployment without custom runner scripts

--- client/build-runner.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-import { execSync } from 'child_process';
-
-console.log('[Client Build Runner] Building client...');
-execSync('npx vite build', { stdio: 'inherit' });
-console.log('[Client Build Runner] Client build succeeded.');
